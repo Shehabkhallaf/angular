c836bf7dae84ef4587f0b35a88b6e00d313b1cee
shehabkhallaf <shehabkhallaf122@gmail.com>
2022-02-26 01:46:20 +0200

    guard

---
+++ src/app/auth-guard.guard.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuardGuard implements CanActivate {
+  isLoggedIn : boolean = true ;
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+      if(this.isLoggedIn){
+        return true ;
+      }
+      window.alert('you dont have permission to view this page ,please login in first')
+    return false;
+  }
+  
+}
